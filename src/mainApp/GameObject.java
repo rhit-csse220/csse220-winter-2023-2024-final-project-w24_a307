1c0c9fb88bff82c036328d46a9fd0a8dfa0efdeb
rhit-yangjc1 <yangjc1@rose-hulman.edu>
2024-01-29 14:48:17 -0500

    adding abstract gameobject methods

--- src/mainApp/GameObject.java
+++ src/mainApp/GameObject.java
@@ -1,7 +1,13 @@
 package mainApp;
 
+import java.awt.Graphics;
+
 public abstract class GameObject {
 	protected int velX, velY, x, y, length, width;
 	public abstract void handlePickup();
-	
+	public abstract void drawOn(Graphics g);
+	public boolean overlapsWith(GameObject other)
+	{
+		return false; // to be implemented
+	}
 }
