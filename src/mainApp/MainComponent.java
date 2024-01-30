2544b7fe2b56ecdf166513f8796f7a21f89dd6b7
rhit-yangjc1 <yangjc1@rose-hulman.edu>
2024-01-30 13:27:27 -0500

    change

--- src/mainApp/MainComponent.java
+++ src/mainApp/MainComponent.java
@@ -1,5 +1,7 @@
 package mainApp;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
@@ -7,5 +9,24 @@ import javax.swing.JComponent;
 public class MainComponent extends JComponent {
 	private Hero hero;
 	private ArrayList<GameObject> gameObjects;
-	
+	public void tick()
+	{
+		hero.update();
+		for(GameObject object : gameObjects)
+		{
+			object.update();
+		}
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		for(GameObject object : gameObjects)
+		{
+			object.drawOn(g);
+		}
+		// TODO: Draw raindrops
+
+	}
 }
