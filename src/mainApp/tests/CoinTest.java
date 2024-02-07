b102b587b1dc8255f49063050651effb13ec99da
rhit-rohan <maliper@rose-hulman.edu>
2024-02-07 14:28:03 -0500

    more tests

--- src/mainApp/tests/CoinTest.java
+++ src/mainApp/tests/CoinTest.java
@@ -4,11 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import mainApp.domain.Barrier;
+import mainApp.domain.Coin;
+import mainApp.domain.Hero;
+
 public class CoinTest {
+	Hero h = new Hero();
+	Hero h1 = new Hero(30, 10, 100, 0, 20, 40, 0, 5);
 
 	@Test
-	public void testOverlapsWith() {
-		fail("Not yet implemented");
+	public void testOverlapsWithN01() {
+		if((new Coin(20, 40)).overlapsWith(h)) {
+			assertEquals(1, h.getScore());
+		}
 	}
+	
+	@Test
+	public void testOverlapsWithN02() {
+		if((new Coin(30, 70)).overlapsWith(h)) {
+			assertEquals(2, h.getScore());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN03() {
+		if((new Coin(200, 150)).overlapsWith(h)) {
+			assertEquals(3, h.getScore());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN04() {
+		if((new Coin(300, 250)).overlapsWith(h)) {
+			assertEquals(4, h.getScore());
+		}
+	}
+	
 
 }
--- src/mainApp/tests/MissileTest.java
+++ src/mainApp/tests/MissileTest.java
@@ -4,11 +4,42 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import mainApp.domain.Barrier;
+import mainApp.domain.Coin;
+import mainApp.domain.Hero;
+import mainApp.domain.Missile;
+
 public class MissileTest {
 
+	Hero h = new Hero();
+	Hero h1 = new Hero(30, 10, 100, 0, 20, 40, 0, 5);
+
 	@Test
-	public void testOverlapsWith() {
-		fail("Not yet implemented");
+	public void testOverlapsWithN01() {
+		if((new Missile(20, 40)).overlapsWith(h)) {
+			assertEquals(1, h.getScore());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN02() {
+		if((new Missile(30, 70)).overlapsWith(h)) {
+			assertEquals(2, h.getScore());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN03() {
+		if((new Missile(200, 150)).overlapsWith(h)) {
+			assertEquals(3, h.getScore());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN04() {
+		if((new Missile(300, 250)).overlapsWith(h)) {
+			assertEquals(4, h.getScore());
+		}
 	}
 
 }
