b2cdcb143f2a062ec897639d2d7bc72d4bb94ba8
rhit-rohan <maliper@rose-hulman.edu>
2024-02-07 13:33:28 -0500

    Barrier test and hero

--- src/mainApp/tests/BarrierTest.java
+++ src/mainApp/tests/BarrierTest.java
@@ -4,11 +4,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import mainApp.domain.Barrier;
+import mainApp.domain.Hero;
+
 public class BarrierTest {
+	Hero h = new Hero();
+	Hero h1 = new Hero(30, 10, 100, 0, 20, 40, 0, 5);
 
 	@Test
-	public void testOverlapsWith() {
-		fail("Not yet implemented");
+	public void testOverlapsWithN01() {
+		if((new Barrier(20, 40, 30, Math.PI/2)).overlapsWith(h)) {
+			assertEquals(0, h.getVelX());
+		}
 	}
+	
+	@Test
+	public void testOverlapsWithN02() {
+		if((new Barrier(70, 50, 9, Math.PI/2)).overlapsWith(h)) {
+			assertEquals(0, h.getVelX());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN03() {
+		if((new Barrier(200, 350, 100, Math.PI/2)).overlapsWith(h)) {
+			assertEquals(0, h.getVelX());
+		}
+	}
+	
+	@Test
+	public void testOverlapsWithN04() {
+		if((new Barrier(300, 150, 55, Math.PI/2)).overlapsWith(h)) {
+			assertEquals(0, h.getVelX());
+		}
+	}
+	
 
 }
