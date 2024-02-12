aa2d5efd102a9840841f21faa4a581a2f34514db
rhit-macrowo <macrowo@rose-hulman.edu>
2024-02-12 14:38:57 -0500

    Merge branch 'master' of https://github.com/rhit-csse220/csse220-winter-2023-2024-final-project-w24_a307.git

--- src/mainApp/domain/ShieldPowerUp.java
+++ src/mainApp/domain/ShieldPowerUp.java
@@ -1,6 +1,12 @@
 package mainApp.domain;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ShieldPowerUp extends PowerUp {
 
@@ -18,6 +24,15 @@ public class ShieldPowerUp extends PowerUp {
 
 	@Override
 	public void drawOn(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File("images/shield.png"));
+			g2.drawImage(img, x, y, SHIELD_WIDTH, SHIELD_HEIGHT, null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		// TODO Auto-generated method stub
 		
 	}
