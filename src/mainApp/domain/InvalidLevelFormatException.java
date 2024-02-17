e72fbb0996616938639b88628a14cba9eb74e398
rhit-yangjc1 <yangjc1@rose-hulman.edu>
2024-02-16 21:33:11 -0500

    levelformatexception doc

--- src/mainApp/domain/InvalidLevelFormatException.java
+++ src/mainApp/domain/InvalidLevelFormatException.java
@@ -1,5 +1,9 @@
 package mainApp.domain;
-
+/*
+ * Class: InvalidLevelFormatException
+ * @author Joshua Yang
+ * Purpose: custom exception for invalid level format
+ */
 public class InvalidLevelFormatException extends Exception {
 
 }
