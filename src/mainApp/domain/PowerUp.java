f56d6291a1512ec59b00ee35ca71912b8e957762
rhit-macrowo <156109402+rhit-macrowo@users.noreply.github.com>
2024-02-16 21:17:59 -0500

    PowerUp.java - javadocs

--- src/mainApp/domain/PowerUp.java
+++ src/mainApp/domain/PowerUp.java
@@ -1,10 +1,16 @@
 package mainApp.domain;
-
+/**
+ * Class: PowerUp
+ * @author Joshua Yang
+ * <br>Purpose: Used as an abstract version of power ups
+ * <br>Restrictions: is an abstract class, not to be used by itself
+ */
 public abstract class PowerUp extends GameObject {
-
+	/*
+	 *  ensures: initializes powerup abstact class
+	 */
 	public PowerUp(int x, int y, int width, int height) {
 		super(0, 0, x, y, width, height);
-		// TODO Auto-generated constructor stub
-	}
+	}//PowerUp
 
 }
