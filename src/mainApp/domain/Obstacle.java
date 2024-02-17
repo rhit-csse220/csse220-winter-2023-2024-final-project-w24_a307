b94e961c3b4af4a497055701caddfdcf676d30c3
rhit-macrowo <156109402+rhit-macrowo@users.noreply.github.com>
2024-02-16 21:16:07 -0500

    Obstacle.java - javadocs

--- src/mainApp/domain/Obstacle.java
+++ src/mainApp/domain/Obstacle.java
@@ -1,10 +1,17 @@
 package mainApp.domain;
 
 import java.awt.Graphics;
-
+/**
+ * Class: Obstacle
+ * @author Joshua Yang
+ * <br>Purpose: Used as an absrtact class for all obstacles
+ * <br>Restrictions: is an abstract class, not to be used by itself
+ */
 public abstract class Obstacle extends GameObject{
-
+	/*
+	 * ensures: initializes default objects
+	 */
 	public Obstacle(int velX, int velY, int x, int y, int width, int height) {
 		super(velX, velY, x, y, width, height);
-	}
+	}//Obstacle
 }
