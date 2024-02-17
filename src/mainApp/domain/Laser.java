130aaca41c19262644a8f9635b22914bb0a67028
rhit-macrowo <156109402+rhit-macrowo@users.noreply.github.com>
2024-02-16 21:10:29 -0500

    Laser.java + javadocs

--- src/mainApp/domain/Laser.java
+++ src/mainApp/domain/Laser.java
@@ -3,7 +3,16 @@ package mainApp.domain;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
+/**
+ * Class: Laser
+ * @author Joshua Yang
+ * <br>Purpose: Used to hold information about the laser object
+ * <br>Restrictions: none
+ * <br>For example: 
+ * <pre>
+ *    obstacles.add(new Laser(s.nextInt()));
+ * </pre>
+ */
 public class Laser extends Obstacle{
 	private int countdown;
 	private boolean isOn;
@@ -11,12 +20,17 @@ public class Laser extends Obstacle{
 	public static final int LASER_HEIGHT = 10;
 	public static final int TIME_BEFORE_ON = 100;
 	public static final int DURATION = 10;
+	/*
+ 	 * ensures: initializes laser object
+   	 */
 	public Laser(int y) {
 		super(0, 0, 0, y, LASER_WIDTH, LASER_HEIGHT);
 		countdown = TIME_BEFORE_ON;
 		isOn = false;
-	}
-
+	} //Laser
+	/* --- overridden drawOn method ---
+	 * ensures: laser is drawn different depending on if on or off.
+	 */
 	@Override
 	public void drawOn(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
@@ -30,8 +44,12 @@ public class Laser extends Obstacle{
 			g2.fillRect(0, y, LASER_WIDTH, LASER_HEIGHT);
 			g2.fillRect(MainApp.FRAME_WIDTH-LASER_WIDTH-15, y, LASER_WIDTH, LASER_HEIGHT);
 		} //-15 is because otherwise it loads off the screen for unknown reasons
-	}
-	
+	} // drawOn
+	/* --- laser update method ---
+	 * ensures: laser barrier is updated properly:
+	 * uses variable 'countdown' to tell when to turn on and off
+	 * - countdown set by DURATION
+	 */
 	public void update()
 	{
 		super.update();
@@ -47,8 +65,10 @@ public class Laser extends Obstacle{
 		}
 		else
 			countdown--;
-	}
-	
+	} // update
+	/* --- laser overlapsWith method ---
+	 * ensures: barrier only considered as overlapping if  isOn = true.
+	 */
 	@Override
 	public boolean overlapsWith(Hero hero)
 	{
@@ -60,9 +80,9 @@ public class Laser extends Obstacle{
 			return true;
 		}
 	return false;
-	}
+	} // overlapsWith
 	public void turnOn() //for testing, delete later
 	{
 		isOn = true;
-	}
+	} // turnOn
 }
